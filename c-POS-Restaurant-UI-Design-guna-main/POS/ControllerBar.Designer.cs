018dab3cb412ee374ab40565b861f779a1fbe7e7
Van Tin <vantin22112003@gmail.com>
2023-11-07 13:31:38 +0700

    add

--- c-POS-Restaurant-UI-Design-guna-main/POS/ControllerBar.Designer.cs
+++ c-POS-Restaurant-UI-Design-guna-main/POS/ControllerBar.Designer.cs
@@ -29,21 +29,35 @@ namespace POS
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(352, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 31;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // ControllerBar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1056, 691);
+            this.Controls.Add(this.button1);
             this.Location = new System.Drawing.Point(0, 0);
             this.Name = "ControllerBar";
             this.Text = "ControllerBar";
             this.Load += new System.EventHandler(this.ControllerBar_Load);
+            this.Controls.SetChildIndex(this.button1, 0);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
