f6eadc2ab82b58435a1d57b3431e130f1ad2f914
cogi2996 <bidang.tuyloan@gmail.com>
2023-11-07 11:07:34 +0700

    'add'

--- c-POS-Restaurant-UI-Design-guna-main/POS/Service.Designer.cs
+++ c-POS-Restaurant-UI-Design-guna-main/POS/Service.Designer.cs
@@ -29,10 +29,17 @@ namespace POS
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Service
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Service";
             this.Text = "Service";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- c-POS-Restaurant-UI-Design-guna-main/POS/Service.cs
+++ c-POS-Restaurant-UI-Design-guna-main/POS/Service.cs
@@ -16,5 +16,7 @@ namespace POS
         {
             InitializeComponent();
         }
+
+        
     }
 }
