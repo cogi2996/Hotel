9cd0aed6c354aab06b0622f5b451895582423b0c
cogi2996 <bidang.tuyloan@gmail.com>
2023-11-07 13:36:26 +0700

    add

--- c-POS-Restaurant-UI-Design-guna-main/POS/ControllerBar.Designer.cs
+++ c-POS-Restaurant-UI-Design-guna-main/POS/ControllerBar.Designer.cs
@@ -29,21 +29,36 @@ namespace POS
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(267, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "label1";
             // 
             // ControllerBar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1056, 691);
+            this.Controls.Add(this.label1);
             this.Location = new System.Drawing.Point(0, 0);
             this.Name = "ControllerBar";
             this.Text = "ControllerBar";
             this.Load += new System.EventHandler(this.ControllerBar_Load);
+            this.Controls.SetChildIndex(this.label1, 0);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
