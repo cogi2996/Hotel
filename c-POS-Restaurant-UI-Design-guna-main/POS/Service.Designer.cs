9850ef481d66430485d4d0b1f3a52633e455fd52
Van Tin <vantin22112003@gmail.com>
2023-11-07 11:03:19 +0700

    test

--- c-POS-Restaurant-UI-Design-guna-main/POS/Service.Designer.cs
+++ c-POS-Restaurant-UI-Design-guna-main/POS/Service.Designer.cs
@@ -29,12 +29,44 @@ namespace POS
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txt_1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_1
+            // 
+            this.txt_1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_1.DefaultText = "";
+            this.txt_1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txt_1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txt_1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txt_1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_1.Location = new System.Drawing.Point(243, 166);
+            this.txt_1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_1.Name = "txt_1";
+            this.txt_1.PasswordChar = '\0';
+            this.txt_1.PlaceholderText = "";
+            this.txt_1.SelectedText = "";
+            this.txt_1.Size = new System.Drawing.Size(229, 48);
+            this.txt_1.TabIndex = 0;
+            this.txt_1.TextChanged += new System.EventHandler(this.txt_1_TextChanged);
+            // 
+            // Service
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_1);
+            this.Name = "Service";
             this.Text = "Service";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2TextBox txt_1;
     }
 }
