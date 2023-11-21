e546cd2a126080f8d985d60229ea0228e703d2f1
cogi2996 <bidang.tuyloan@gmail.com>
2023-11-21 11:28:14 +0700

    'add'

--- c-POS-Restaurant-UI-Design-guna-main/POS/Program.cs
+++ c-POS-Restaurant-UI-Design-guna-main/POS/Program.cs
@@ -16,7 +16,7 @@ namespace POS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new checkout());
+            Application.Run(new Form1());
         }
     }
 }
